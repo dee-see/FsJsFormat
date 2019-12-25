4ffa6855dbddfca6c75aac5c64a025535aa98170
dee-see <dominic.couture@gmail.com>
2019-12-25 10:06:21 -0500

    change how text is passed around

--- Program.fs
+++ Program.fs
@@ -10,100 +10,94 @@ type State =
     | InRegexFlags of char list
     | InRegexCharacterClass of char list
 
-type Writer(textWriter) = 
-    member this.TextWriter : IO.TextWriter = textWriter
+type FormatterChars =
+    | Empty
+    | Letter of char
+    | Text of string
 
-    member this.Append (str : string) =
-        this.TextWriter.Write(str)
-        this
-
-    member this.Append (c : char) =
-        this.TextWriter.Write(c)
-        this
-
-    member this.AppendLine() =
-        this.Append('\n')
-    
-    interface IDisposable with
-        member this.Dispose() =
-            this.TextWriter.Dispose()
-
-let format textWriter (input: string) =
+let format (textWriter : IO.TextWriter) (input: string) =
     let newLineChars = [ ';'; ',' ]
     let indentChars = [ '{' ]
     let unindentChars = [ '}' ]
     let stringDelimChars = [ '\''; '`'; '"' ]
-    let pad indentLevel = String(' ', (max (indentLevel * 4) 0)) // Shouldn't need the max, but errors happen...
     let newLine = Environment.NewLine
+    let pad indentLevel = String(' ', (max (indentLevel * 4) 0)) // Shouldn't need the max, but errors happen...
 
-    let rec formatChars (w: Writer) indentLevel charList =
+    let rec formatChars formattedChars indentLevel charList =
+        match formattedChars with
+        | Letter l -> textWriter.Write(l)
+        | Text t -> textWriter.Write(t)
+        | Empty -> ()
+
         match charList with
         | AtBeginningOfLine x ->
             match x with
-            | c :: rst when Char.IsWhiteSpace(c) -> formatChars w indentLevel (AtBeginningOfLine rst)
-            | _ -> formatChars w indentLevel (InCode x)
+            | c :: rst when Char.IsWhiteSpace(c) -> formatChars Empty indentLevel (AtBeginningOfLine rst)
+            | _ -> formatChars Empty indentLevel (InCode x)
         | InCode x ->
             match x with
-            | '/' :: '/' :: rst -> formatChars (w.Append("//")) indentLevel (InSingleLineComment rst)
-            | '/' :: '*' :: rst -> formatChars (w.Append("/*")) indentLevel (InMultiLineComment rst)
-            | '/' :: rst -> formatChars (w.Append("/")) indentLevel (InRegex rst)
-            | c :: rst when List.contains c newLineChars -> formatChars (w.Append((sprintf "%O%s%s" c newLine (pad indentLevel)))) indentLevel (AtBeginningOfLine rst)
+            | '/' :: '/' :: rst -> formatChars (Text "//") indentLevel (InSingleLineComment rst)
+            | '/' :: '*' :: rst -> formatChars (Text "/*") indentLevel (InMultiLineComment rst)
+            | '/' :: rst -> formatChars (Letter '/') indentLevel (InRegex rst)
+            | c :: rst when List.contains c newLineChars -> formatChars (Text (sprintf "%O%s%s" c newLine (pad indentLevel))) indentLevel (AtBeginningOfLine rst)
             | c :: rst when List.contains c indentChars -> 
                 let newIndent = indentLevel + 1
-                formatChars (w.Append((sprintf "%O%s%s" c newLine (pad newIndent)))) newIndent (InCode rst)
+                formatChars (Text ((sprintf "%O%s%s" c newLine (pad newIndent)))) newIndent (InCode rst)
             | c1 :: rst1 when List.contains c1 unindentChars -> 
                 let newIndent = indentLevel - 1
                 match rst1 with
-                | c2 :: rst2 when List.contains c2 newLineChars -> formatChars (w.Append((sprintf "%s%s%O%O%s%s" newLine (pad newIndent) c1 c2 newLine (pad newIndent)))) newIndent (InCode rst2)
-                | c2 :: rst2 -> formatChars (w.Append((sprintf "%s%s%O%s%s%O" newLine (pad newIndent) c1 newLine (pad newIndent) c2))) newIndent (InCode rst2)
+                | c2 :: rst2 when List.contains c2 newLineChars -> formatChars (Text ((sprintf "%s%s%O%O%s%s" newLine (pad newIndent) c1 c2 newLine (pad newIndent)))) newIndent (InCode rst2)
+                | c2 :: rst2 -> formatChars (Text ((sprintf "%s%s%O%s%s%O" newLine (pad newIndent) c1 newLine (pad newIndent) c2))) newIndent (InCode rst2)
                 | [] -> ()
-            | c :: rst when List.contains c stringDelimChars -> formatChars (w.Append(c)) indentLevel (InString(c, rst))
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InCode rst)
+            | c :: rst when List.contains c stringDelimChars -> formatChars (Letter c) indentLevel (InString(c, rst))
+            | c :: rst -> formatChars (Letter c) indentLevel (InCode rst)
             | [] -> ()
         | InSingleLineComment x ->
             match x with
-            | '\n' :: rst -> formatChars (w.AppendLine()) indentLevel (InCode rst)
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InSingleLineComment rst)
+            | '\n' :: rst -> formatChars (Text newLine) indentLevel (InCode rst)
+            | c :: rst -> formatChars (Letter c) indentLevel (InSingleLineComment rst)
             | [] ->()
         | InMultiLineComment x ->
             match x with
-            | '*' :: '/' :: rst -> formatChars (w.Append("*/")) indentLevel (InCode rst)
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InMultiLineComment rst)
+            | '*' :: '/' :: rst -> formatChars (Text "*/") indentLevel (InCode rst)
+            | c :: rst -> formatChars (Letter c) indentLevel (InMultiLineComment rst)
             | [] -> ()
         | InString (delim, x) ->
             match x with
-            | '\\' :: c :: rst when c = delim -> formatChars (w.Append("\\").Append(delim)) indentLevel (InString(delim, rst))
-            | c :: rst when c = delim -> formatChars (w.Append(delim)) indentLevel (InCode rst)
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InString(delim, rst))
+            | '\\' :: c :: rst when c = delim -> formatChars (Text (sprintf "\\%O" delim)) indentLevel (InString(delim, rst))
+            | c :: rst when c = delim -> formatChars (Letter delim) indentLevel (InCode rst)
+            | c :: rst -> formatChars (Letter c) indentLevel (InString(delim, rst))
             | [] -> ()
         | InRegex x ->
             match x with
-            | '\\' :: c :: rst -> formatChars (w.Append('\\').Append(c)) indentLevel (InRegex rst)
-            | '/' :: c :: rst when Char.IsLetter(c) -> formatChars (w.Append('/').Append(c)) indentLevel (InRegexFlags rst)
-            | '/' :: rst -> formatChars (w.Append('/')) indentLevel (InCode rst)
-            | '[' :: rst -> formatChars (w.Append('[')) indentLevel (InRegexCharacterClass rst)
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InRegex rst)
+            | '\\' :: c :: rst -> formatChars (Text (sprintf "\\%O" c)) indentLevel (InRegex rst)
+            | '/' :: c :: rst when Char.IsLetter(c) -> formatChars (Text (sprintf "/%O" c)) indentLevel (InRegexFlags rst)
+            | '/' :: rst -> formatChars (Letter '/') indentLevel (InCode rst)
+            | '[' :: rst -> formatChars (Letter '[') indentLevel (InRegexCharacterClass rst)
+            | c :: rst -> formatChars (Letter c) indentLevel (InRegex rst)
             | [] -> ()
         | InRegexFlags x ->
             match x with
-            | c :: rst when Char.IsLetter(c) -> formatChars (w.Append(c)) indentLevel (InRegexFlags rst)
-            | _ -> formatChars w indentLevel (InCode x)
+            | c :: rst when Char.IsLetter(c) -> formatChars (Letter c) indentLevel (InRegexFlags rst)
+            | _ -> formatChars Empty indentLevel (InCode x)
         | InRegexCharacterClass x ->
             match x with
-            | '\\' :: c :: rst -> formatChars (w.Append('\\').Append(c)) indentLevel (InRegexCharacterClass rst)
-            | ']' :: rst -> formatChars (w.Append(']')) indentLevel (InRegex rst)
-            | c :: rst -> formatChars (w.Append(c)) indentLevel (InRegexCharacterClass rst)
+            | '\\' :: c :: rst -> formatChars (Text (sprintf "\\%O" c)) indentLevel (InRegexCharacterClass rst)
+            | ']' :: rst -> formatChars (Letter ']') indentLevel (InRegex rst)
+            | c :: rst -> formatChars (Letter c) indentLevel (InRegexCharacterClass rst)
             | [] -> ()
 
-    use writer = new Writer(textWriter)
     List.ofSeq input
     |> AtBeginningOfLine
-    |> formatChars writer 0
+    |> formatChars Empty 0
 
 [<EntryPoint>]
 let main argv =
     match argv with
     | [| file |] -> IO.File.ReadAllText(file) |> format Console.Out
-    | [| file; "--save" |] -> IO.File.ReadAllText(file) |> format (IO.File.CreateText(file))
+    | [| file; "--save" |] -> 
+        let text = IO.File.ReadAllText(file)
+        use writer = IO.File.CreateText(file)
+        format writer text
     | _ -> eprintfn "Usage: FsJsFormat file.js [--save]"
     0
