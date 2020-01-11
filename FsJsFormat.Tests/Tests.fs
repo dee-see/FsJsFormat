adebcac187abb760344eebbc802e80259492f182
dee-see <dominic.couture@gmail.com>
2020-01-10 22:05:57 -0500

    Add first test

---
+++ FsJsFormat.Tests/Tests.fs
@@ -0,0 +1,23 @@
+module Tests
+
+open System
+open System.IO
+open Xunit
+
+[<Fact>]
+let ``Nested objects`` () =
+    use sw = new StringWriter()
+    let input = "{\"a\":{\"b\":{\"c\":4}}}\n"
+    FsJsFormat.format sw input
+    let expected = 
+        [ "{"
+          "    \"a\":{"
+          "        \"b\":{"
+          "            \"c\":4"
+          "        }"
+          "    }"
+          "}"
+          ""
+          "" ] // FIXME the test in hacked to make sure it passes even with the extra new lines...
+        |> String.concat Environment.NewLine
+    Assert.Equal(expected, (sw.ToString()))
